2d4291bd7f644c7d9c50ade8aed6b4cd6cc25850
Eugene Gorelenko <oldtoy480@gmail.com>
2022-12-19 13:54:25 +0300

    Complete gekko movement
    Камера теперь поворачивается за геконном, куда он смотрит.

--- Assets/Scripts/GekkoScript.cs
+++ Assets/Scripts/GekkoScript.cs
@@ -4,40 +4,11 @@ using UnityEngine;
 
 public class GekkoScript : MonoBehaviour
 {
-    [SerializeField] private Vector3 offsetCamera;
-    [SerializeField] private float radius = 0.4f;
-    [SerializeField] private float angularSpeed = 4f;
-
-    private float _angle = 0f;
     private Camera _camera;
     
     private void Start()
     {
         _camera = Camera.main;
-    }
-
-    private void Update()
-    {
-        _camera.transform.position = transform.position + offsetCamera;
         _camera.transform.LookAt(transform);
-
-        var positionX = transform.position.x + Mathf.Cos(_angle) * radius;
-        var positionZ = transform.position.z + Mathf.Sin(_angle) * radius;
-
-        if (Input.GetKey(KeyCode.LeftArrow))
-        {
-            offsetCamera = (new Vector3(positionX, 0, positionZ) * Time.deltaTime) + offsetCamera;
-            _angle += Time.deltaTime * angularSpeed;
-        }
-        if (Input.GetKey(KeyCode.RightArrow))
-        {
-            offsetCamera = -(new Vector3(positionX, 0, positionZ) * Time.deltaTime) + offsetCamera;
-            _angle -= Time.deltaTime * angularSpeed;
-        }
-
-        if (_angle > 360 || _angle < -360)
-        {
-            _angle = 0;
-        }
     }
 }
